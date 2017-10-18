93c492df515ca3f6385237883ce002a7834d6fa3
Adrien Matissart <a.matissart@qwantresearch.com>
2017-10-18 15:53:56 +0200

    Add docstring

--- api/search.py
+++ api/search.py
@@ -2,6 +2,12 @@ from apistar.exceptions import ValidationError
 
 
 def search_address(q: str):
+    """
+    Check if the query should display a map
+
+    :param q: (str) - User query
+    :return: (dict) - JSON response
+    """
     if q is None:
         # value is None if the query param is absent in request
         raise ValidationError({'q': '"q" is required'})
